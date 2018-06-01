7e29149939f261879b50ccad32c6696034b4c925
Cash Wu <cash@cashwu.com>
2018-06-01 15:39:24 +0800

    modify FileContentˆResultAssertions

--- Src/Baymax/FileContentResultAssertions.cs
+++ Src/Baymax/FileContentResultAssertions.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using ExpectedObjects;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Baymax
 {
@@ -11,6 +14,34 @@ namespace Baymax
         {
             this._fileContentResult = fileContentResult;
             this._controller = controller;
+        }
+
+        public FileContentResultAssertions<TController> WithContentType(string expectedContentType)
+        {
+            this._fileContentResult.ContentType.Should().Be(expectedContentType);
+
+            return this;
+        }
+        
+        public FileContentResultAssertions<TController> WithFileContents(byte[] expectedFileContents)
+        {
+            expectedFileContents.ToExpectedObject().ShouldEqual(_fileContentResult.FileContents);
+
+            return this;
+        }
+        
+        public FileContentResultAssertions<TController> WithFileDownloadName(string expectedFileDownloadName)
+        {
+            _fileContentResult.FileDownloadName.Should().Be(expectedFileDownloadName);
+
+            return this;
+        }
+        
+        public FileContentResultAssertions<TController> WithLastModified(DateTimeOffset expectedLastModified)
+        {
+            _fileContentResult.LastModified.Value.Should().Be(expectedLastModified);
+
+            return this;
         }
     }
 }
