ec8163a9e07bab9ae27fb0bf4c892334c475a555
Cash Wu <cash@cashwu.com>
2018-06-01 15:09:34 +0800

    modify PartialViewResultAssertions

--- Src/Baymax/PartialViewResultAssertions.cs
+++ Src/Baymax/PartialViewResultAssertions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExpectedObjects;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Baymax
 {
@@ -11,6 +13,43 @@ namespace Baymax
         {
             _partialViewResult = partialViewResult;
             _controller = controller;
+        }
+        
+        public PartialViewResultAssertions<TController> WithModel<T>(T expected)
+        {
+            _partialViewResult.Model.Should().BeAssignableTo<T>();
+
+            expected.ToExpectedObject().ShouldEqual(_partialViewResult.Model);
+
+            return this;
+        }
+
+        public PartialViewResultAssertions<TController> WithViewName(string viewName)
+        {
+            _partialViewResult.ViewName.Should().Be(viewName);
+
+            return this;
+        }
+
+        public PartialViewResultAssertions<TController> WithViewBag(string key, object expectedValue)
+        {
+            expectedValue.ToExpectedObject().ShouldEqual(_partialViewResult.ViewData[key]);
+
+            return this;
+        }
+
+        public PartialViewResultAssertions<TController> WithNotTempData()
+        {
+            _partialViewResult.TempData.Count.Should().Be(0);
+
+            return this;
+        }
+
+        public PartialViewResultAssertions<TController> WithTempData(string key, object expectedValue)
+        {
+            expectedValue.ToExpectedObject().ShouldEqual(_partialViewResult.TempData[key]);
+
+            return this;
         }
     }
 }
