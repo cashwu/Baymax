c07ebaa7210b65c4f04ea9fb0c409c676ac630b9
Cash Wu <cash@cashwu.com>
2018-10-03 21:53:40 +0800

    add general background service

--- Src/Baymax/Extension/ConfigureServiceExtensions.cs
+++ Src/Baymax/Extension/ConfigureServiceExtensions.cs
@@ -7,15 +7,36 @@ using Baymax.Model.Config;
 using Baymax.Services;
 using Baymax.Services.Interface;
 using Baymax.Util;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Baymax.Extension
 {
     public static class ConfigureServiceExtensions
     {
+        public static void AddBackgroundServiceFor(this IServiceCollection services, params Type[] type)
+        {
+            foreach (var t in type)
+            {
+                if (t.GetInterfaces().First() != typeof(IBackgroundProcessService))
+                {
+                    throw new ArgumentException($"Not implement type {nameof(IBackgroundProcessService)}");
+                }
+
+                services.AddScoped(t);
+                services.AddSingleton(typeof(IHostedService), typeof(BaymaxBackgroundService<>).MakeGenericType(t));
+            }
+        }
+
         public static void AddDefaultConfigMapping(this IServiceCollection services, IConfiguration configuration)
         {
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof(IConfiguration));
+            }
+
             var types = Reflection.GetAssembliesTypeOf<IConfig>();
 
             foreach (var type in types)
@@ -48,28 +69,33 @@ namespace Baymax.Extension
             }
         }
 
-        public static void AddRegisterAllTypeFor(this IServiceCollection services,
-                                                 Func<Assembly, bool> assemblyCondition,
-                                                 Func<TypeInfo, bool> typeNameCondition,
-                                                 Dictionary<string, ServiceLifetime> typeLifetimeLookup)
+        public static void AddRegisterServiceTypeOf(this IServiceCollection services, string prefixAssemblyName)
         {
-            if (typeNameCondition == null)
+            if (string.IsNullOrEmpty(prefixAssemblyName))
             {
-                return;
+                throw new ArgumentNullException(nameof(prefixAssemblyName));
             }
 
+            services.AddRegisterAllTypeFor(f => f.FullName.StartsWith(prefixAssemblyName),
+                                           f => f.Name.EndsWith("Service"));
+        }
+
+        private static void AddRegisterAllTypeFor(this IServiceCollection services,
+                                                  Func<Assembly, bool> assemblyCondition,
+                                                  Func<TypeInfo, bool> typeNameCondition)
+        {
             var assemblies = Reflection.GetAssembliesTypeOf(assemblyCondition, typeNameCondition);
+
             foreach (var @class in assemblies)
             {
                 foreach (var @interface in @class.GetInterfaces().Where(a => typeNameCondition.Invoke(a.GetTypeInfo())))
                 {
-                    var lifetime = ServiceLifetime.Scoped;
-                    if (typeLifetimeLookup.ContainsKey(@class.Name))
+                    if (@interface == typeof(IBackgroundProcessService))
                     {
-                        lifetime = typeLifetimeLookup[@class.Name];
+                        continue;
                     }
 
-                    services.Add(new ServiceDescriptor(@interface, @class.AsType(), lifetime));
+                    services.Add(new ServiceDescriptor(@interface, @class.AsType(), ServiceLifetime.Scoped));
                 }
             }
         }
