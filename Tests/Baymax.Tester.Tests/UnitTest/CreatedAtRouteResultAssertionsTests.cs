c0c91eeb5955bd21e6e514a70b929db5ad2e2a7b
Cash Wu <cash@cashwu.com>
2018-11-12 16:46:00 +0800

    add CreateAtRouteResultAssertion tests

---
+++ Tests/Baymax.Tester.Tests/UnitTest/CreatedAtRouteResultAssertionsTests.cs
@@ -0,0 +1,46 @@
+using Baymax.Tester.UnitTest;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Baymax.Tester.Tests.UnitTest
+{
+    public class CreatedAtRouteResultAssertionsTests
+    {
+        [Fact]
+        public void WithRouteValue_Value()
+        {
+            new CreatedAtRoutedResultController()
+                    .AsTester()
+                    .Action(c => c.Action())
+                    .ShouldBeCreatedAtRouteResult()
+                    .WithRouteValue("id", 1)
+                    .WithValue(new { data = 123 });
+        }
+
+        
+        [Fact]
+        public void WithRouteName_RouteValue_Value()
+        {
+            new CreatedAtRoutedResultController()
+                    .AsTester()
+                    .Action(c => c.Action2())
+                    .ShouldBeCreatedAtRouteResult()
+                    .WithRouteName("routeName")
+                    .WithRouteValue("id", 1)
+                    .WithValue(new { data = 123 });
+        }
+    }
+
+    internal class CreatedAtRoutedResultController : Controller
+    {
+        public IActionResult Action()
+        {
+            return new CreatedAtRouteResult(new { id = 1 }, new { data = 123 });
+        }
+        
+        public IActionResult Action2()
+        {
+            return new CreatedAtRouteResult("routeName", new { id = 1 }, new { data = 123 });
+        }
+    }
+}
